95d245ffa4079b139032344804090a92be3f1cd9
Kc9njzTech <kc9njz.technologies@gmail.com>
2017-02-12 13:53:15 -0600

    Update PITweeter CPU.py
    This is CPU TEMP addition

--- PITweeter CPU.py
+++ PITweeter CPU.py
@@ -1,15 +1,17 @@
-Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
+Python 3.5.2
 >>> 
 
 #!/usr/bin/env python
 import sys
 from twython import Twython
-CONSUMER_KEY = '***************YOUR DATA*****************'
+CONSUMER_KEY = '**********YOUR DATA**********'
 CONSUMER_SECRET = '***************YOUR DATA*****************'
 ACCESS_KEY = '***************YOUR DATA*****************'
 ACCESS_SECRET = '***************YOUR DATA*****************'
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 
-api.update_status(status=sys.argv[1])
+cmd = '/opt/vc/bin/vcgencmd measure_temp'
+line = os.popen(cmd).readline().strip()
+temp = line.split('=')[1].split("'")[0]
+api.update_status(status='My current CPU temperature is '+temp+' C')
